cb00c6973ec7a4d1d3094c90a4990e068bb3d757
jdev92 <juliencavaillon6@gmail.com>
2023-12-01 15:13:54 +0100

    Suppression des heures

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,7 @@ router.post("/createUser", async (req, res) => {
           lastName: lastName,
           firstName: firstName,
           email: email,
-          createdAt: new Date().setHours(0, 0, 0, 0),
+          createdAt: new Date(),
         });
 
         await newUser.save();
